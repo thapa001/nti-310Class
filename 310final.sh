169d28cc87cd112f18b1ae804f30049bb7f9e39f
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-03-22 11:32:29 -0700

    Update 310final.sh

--- 310final.sh
+++ 310final.sh
@@ -36,14 +36,14 @@ gcloud compute instances create ldapautouser2 \
       
 
 #4 #nfs
-gcloud compute instances create networkfilesystem2 \
+gcloud compute instances create nfspart1 \
 --image-family centos-7 \
 --image-project centos-cloud \
 --zone us-east1-b \
 #--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=nti-310Class/networkfilesystem.sh
+--metadata-from-file startup-script=nti-310Class/nfs-a.sh
       
 #5 django
 gcloud compute instances create djangopostgresfinal2 \
