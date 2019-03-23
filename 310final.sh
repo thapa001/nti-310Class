2c31b8ac7151f2b4673259e68b776a7861516fac
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-03-23 13:12:31 -0700

    Update 310final.sh

--- 310final.sh
+++ 310final.sh
@@ -64,7 +64,7 @@ gcloud compute instances create nfsfinal1 \
 #--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=nti-310Class/nfsandldaplient.sh
+--metadata-from-file startup-script=nti-310Class/nfs_and_ldap_client.sh
       
 
 #7 nfs and ldap client- 2
@@ -75,4 +75,4 @@ gcloud compute instances create nfsfinalpart2 \
 #--tags "http-server","https-server" \
 --machine-type f1-micro \
 --scopes cloud-platform \
---metadata-from-file startup-script=nti-310Class/nfsandldaplient.sh
+--metadata-from-file startup-script=nti-310Class/nfs_and_ldap_client.sh
