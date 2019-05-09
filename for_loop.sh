2f73215cd215ab910f6e2412bd26d4653f3cee63
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-05-09 13:28:11 -0700

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #V means remove that matches nagios in the script
-for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios-a );  do 
+for servername in $(gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagiosinstall );  do 
     echo $servername;
     serverip=$( gcloud compute instances list | grep $servername | awk '{print $4}');
     echo $serverip ;
     bash scp_to_nagios.sh $servername $serverip
 done
-gcloud compute ssh --zone us-east4-c mailatpradip8@nagios-a --command='sudo systemctl restart nagios'
+gcloud compute ssh --zone us-east4-c mailatpradip8@nagiosinstall --command='sudo systemctl restart nagios'
