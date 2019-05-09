ed0785c89b5ffb21c8c5efa58bcd850b8e53680a
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-05-09 14:34:40 -0700

    Update for_loop_for_nrpe_install.sh

--- for_loop_for_nrpe_install.sh
+++ for_loop_for_nrpe_install.sh
@@ -1,3 +1,3 @@
 for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios_install );  do 
-    gcloud compute ssh --zone us-east4-c mailatpradip8@$servername --command=' sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/thapa001/nti-310Class/master/nagiosclientconfig.sh && sudo bash nagiosclientconfig.sh'
+    gcloud compute ssh --zone us-east1-b mailatpradip8@$servername --command=' sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/thapa001/nti-310Class/master/nagiosclientconfig.sh && sudo bash nagiosclientconfig.sh'
 done
