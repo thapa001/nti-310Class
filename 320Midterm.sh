4062fe8d384d833488958f8d881a7b066e432d34
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-05-10 13:39:37 -0700

    Update 320Midterm.sh

--- 320Midterm.sh
+++ 320Midterm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 https://github.com/thapa001/nti-310Class.git
-
 #1 create nagios server
 gcloud compute instances create nagiosinstall \
 --image-family centos-7 \
@@ -11,7 +9,6 @@ gcloud compute instances create nagiosinstall \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti-310Class/nagios_install.sh
-
 #2 create cacti server
 gcloud compute instances create cactiinstall \
 --image-family centos-7 \
@@ -21,7 +18,6 @@ gcloud compute instances create cactiinstall \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti-310Class/cacti_install.sh
-
 #3 Rsyslog Server
 #gcloud compute instances create rsyslogserver \
 #--image-family centos-7 \
@@ -30,7 +26,6 @@ gcloud compute instances create cactiinstall \
 #--machine-type f1-micro \
 #--scopes cloud-platform \
 #--metadata-from-file startup-script=nti-310Class/rsyslog_server.sh
-
 #4 postgres and phpPGadmin
 gcloud compute instances create postgresphpadminserver \
 --image-family centos-7 \
@@ -40,7 +35,6 @@ gcloud compute instances create postgresphpadminserver \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti-310Class/postgres_phpadmin.sh
-
 #5 ldap
 gcloud compute instances create ldapserver \
 --image-family centos-7 \
@@ -50,7 +44,6 @@ gcloud compute instances create ldapserver \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti-310Class/ldap.sh
-      
 #6 nfs
 gcloud compute instances create nfs \
 --image-family centos-7 \
@@ -59,7 +52,6 @@ gcloud compute instances create nfs \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti-310Class/nfs-a.sh
-      
 #7 django
 gcloud compute instances create django \
 --image-family centos-7 \
@@ -69,8 +61,6 @@ gcloud compute instances create django \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti-310Class/django_postgres.sh
-      
-
 #8 nfs and ldap client - 1 
 gcloud compute instances create nfspart1 \
 --image-family ubuntu-1804-lts \
@@ -79,8 +69,6 @@ gcloud compute instances create nfspart1 \
 --machine-type f1-micro \
 --scopes cloud-platform \
 --metadata-from-file startup-script=nti-310Class/nfs_and_ldap_client.sh
-      
-
 #9 nfs and ldap client- 2
 gcloud compute instances create nfspart2 \
 --image-family ubuntu-1804-lts \
