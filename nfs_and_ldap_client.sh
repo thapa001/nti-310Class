d0252f25cee9fec58983cd91d65d178fa73aa531
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-05-16 23:58:09 -0700

    Update nfs_and_ldap_client.sh

--- nfs_and_ldap_client.sh
+++ nfs_and_ldap_client.sh
@@ -31,7 +31,7 @@ nslcd nslcd/ldap-starttls boolean false
 libpam-runtime libpam-runtime/profiles multiselect unix, ldap, systemd, capability
 nslcd nslcd/ldap-sasl-authzid string
 ldap-auth-config ldap-auth-config/rootbinddn string cn=ldapadm,dc=nti310,dc=local
-nslcd nslcd/ldap-uris string ldap://ldapautouser2
+nslcd nslcd/ldap-uris string ldap://ldapserver
 nslcd nslcd/ldap-reqcert select
 nslcd nslcd/ldap-sasl-secprops string
 ldap-auth-config ldap-auth-config/ldapns/ldap_version select 3
@@ -46,7 +46,7 @@ ldap-auth-config ldap-auth-config/pam_password select md5
 nslcd nslcd/ldap-sasl-mech select
 nslcd nslcd/ldap-sasl-authcid string
 ldap-auth-config ldap-auth-config/ldapns/base-dn string dc=nti310,dc=local
-ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://ldapautouser2/
+ldap-auth-config ldap-auth-config/ldapns/ldap-server string ldap://ldapserver/
 nslcd nslcd/ldap-binddn string
 ldap-auth-config ldap-auth-config/dblogin boolean false" >> tempfile
 
@@ -67,7 +67,7 @@ echo "account sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/
 
 #edit ldap.conf file to point to proper network
 sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
-sed -i 's,uri ldapi:///,uri ldap://ldapautouser2,g' /etc/ldap.conf #can use something different from "/" as seperator
+sed -i 's,uri ldapi:///,uri ldap://ldapserver,g' /etc/ldap.conf #can use something different from "/" as seperator
 sed -i 's/rootbinddn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/ldap.conf
 
 #restart and enable nscd
