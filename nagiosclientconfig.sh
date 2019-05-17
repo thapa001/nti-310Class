23590400a29a0f56b20034de56737c68b42dc3d5
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-05-16 18:33:58 -0700

    Update nagiosclientconfig.sh

--- nagiosclientconfig.sh
+++ nagiosclientconfig.sh
@@ -5,7 +5,7 @@ wget -O /usr/lib64/nagios/plugins/check_mem.sh https://raw.githubusercontent.com
 chmod +x /usr/lib64/nagios/plugins/check_mem.sh
 systemctl enable nrpe
 systemctl start nrpe
-sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.150.0.2/g' /etc/nagios/nrpe.cfg
+sed -i 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1, 10.138.0.27/g' /etc/nagios/nrpe.cfg
 sed -i "s,command[check_hda1]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/hda1,command[check_disk]=/usr/lib64/nagios/plugins/check_disk -w 20% -c 10% -p /dev/sda1,g" /etc/nagios/nrpe.cfg
 
 #Got a make changes on the vim /etc/nagios/nrpe.cfg and the original lines looks like below. 
