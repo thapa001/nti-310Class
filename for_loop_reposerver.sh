246f1003200922a47283d0eb9c9b52c3b1748e1d
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-06-13 11:11:08 -0700

    Create for_loop_reposerver.sh

---
+++ for_loop_reposerver.sh
@@ -0,0 +1,2 @@
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v reposerver.sh );  do gcloud compute ssh --zone us-west1-b mailatpradip8@$servername --command=' sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/thapa001/nti-310Class/master/clientforrepo.sh && sudo bash clientforrepo.sh'; done
+done
