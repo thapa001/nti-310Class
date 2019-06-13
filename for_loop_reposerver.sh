88f0e0b8fb8e8160de38b29bd30e1b63810af303
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-06-13 11:14:37 -0700

    Update for_loop_reposerver.sh

--- for_loop_reposerver.sh
+++ for_loop_reposerver.sh
@@ -1,2 +1,3 @@
-for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v reposerver.sh );  do gcloud compute ssh --zone us-west1-b mailatpradip8@$servername --command=' sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/thapa001/nti-310Class/master/clientforrepo.sh && sudo bash clientforrepo.sh'; done
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v reposerver.sh );  do 
+    gcloud compute ssh --zone us-west1-b mailatpradip8@$servername --command=' sudo yum -y install wget && sudo wget https://raw.githubusercontent.com/thapa001/nti-310Class/master/clientforrepo.sh && sudo bash clientforrepo.sh'
 done
