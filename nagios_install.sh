8fb12a3643b26e98c8f4c82ff9a0f62b84f4fb23
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-06-13 14:42:27 -0700

    Update nagios_install.sh

--- nagios_install.sh
+++ nagios_install.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-#min nagios install 
+#nagios install 
 #(this is for the first week of configuration.  We will be compiling from source later in the quarter)
 yum -y install nagios
 systemctl enable nagios
@@ -13,17 +13,22 @@ systemctl enable nrpe
 systemctl start nrpe
 yum -y install nagios-plugins-all
 yum -y install nagios-plugins-nrpe
-htpasswd -c /etc/nagios/passwd nagiosadmin #P@ssw0rd1
-usermod -a -G nagios mailatpradip8
-chmod 775 /var/log/nagios/nagios.log 
+htpasswd -b -c /etc/nagios/passwd nagiosadmin #P@ssw0rd1
+chmod 666 /var/log/nagios/nagios.log
+mkdir /etc/nagios/servers
+sed -i '51 s/^#//' /etc/nagios/nagios.cfg
+
+#checking configuration which reads nagios
+/usr/sbin/nagios -v /etc/nagios/nagios.cfg
+systemctl restart nagios
+##verifying the server to navigate ip instance server 
+./generate_config.sh web-a 10.150.0.4
 systemctl restart nagios
 echo "define command{
                                 command_name check_nrpe
                                 command_line /usr/lib64/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$
                                 }" >> /etc/nagios/objects/commands.cfg       
-#checking configuration which reads nagios
-/usr/sbin/nagios -v /etc/nagios/nagios.cfg
-#verifying the server to navigate ip instance server 
-systemctl restart nagios
-#./generate_config.sh web-a 10.150.0.4
+
+usermod -a -G nagios mailatpradip8
+chmod 777 /etc/nagios/servers
 systemctl restart nagios
