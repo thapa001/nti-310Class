56991775ab23c25dcdecd30b613abd9800dced97
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-06-13 16:51:05 -0700

    Update rpmbuildserver.sh

--- rpmbuildserver.sh
+++ rpmbuildserver.sh
@@ -9,13 +9,19 @@ cd ~/
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros  
 cd ~/rpmbuild/SOURCES
 # Not needed but to better automate what is needed.
-git clone https://github.com/nic-instruction/NTI-320.git
-cd NTI-320/
-cp NTI-320/rpm-info/hellow_world_from_source/helloworld-0.1.tar.gz .
-cp NTI-320/rpm-info/hello_world_from_source/helloworld.spec .
-cp NTI-320/rpm-info/hello_world_from_source/hello.spec .
+git clone https://github.com/nic-instruction/custom-nrpe-2019.git
+cd custom-nrpe-2019/
+cp custom-nrpe-2019/rpm-info/hellow_world_from_source/helloworld-0.1.tar.gz .
+cp custom-nrpe-2019/rpm-info/hello_world_from_source/helloworld.spec .
+cp custom-nrpe-2019/rpm-info/hello_world_from_source/hello.spec .
 mv hello.spec ../SPECS
 cd ..
 #Builds rpm 
 rpmbuild -v -bb --clean SPECS/hello.spec
 yum -y install RPMS/x86_64/helloworld-0.1-1.el7.x86_64.rpm 
+
+
+
+
+
+
