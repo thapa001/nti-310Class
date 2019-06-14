f2a26db6fe7f19ce235d030caa0ffdb43b67bf3a
thapa001 <43660000+thapa001@users.noreply.github.com>
2019-06-13 19:52:32 -0700

    Update rpmbuildserver.sh

--- rpmbuildserver.sh
+++ rpmbuildserver.sh
@@ -10,13 +10,13 @@ echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 cd ~/rpmbuild/SOURCES
 # Not needed but to better automate what is needed.
 git clone https://github.com/nic-instruction/custom-nrpe-2019.git
-cd custom-nrpe-2019/
+#cd custom-nrpe-2019/
 cp custom-nrpe-2019/nti-320-plugins-0.1.tar.gz .
 cp custom-nrpe-2019/nti-320-plugins/apachectl_config_example.sh .
 cp custom-nrpe-2019/nti-320-plugins.spec .
-mv nti-320-plugins.spec ../SPECS
+cp nti-320-plugins.spec ../SPECS
 cd ..
 #Builds rpm 
 rpmbuild -v -bb --clean SPECS/nti-320-plugins.spec
-yum -y install RPMS/x86_64/nti-320-plugins-0.1-1.el7.centos.x86_64.rpm 
+yum -y install RPMS/x86_64/nti-320-plugins-0.1-1.el7.x86_64.rpm 
 
